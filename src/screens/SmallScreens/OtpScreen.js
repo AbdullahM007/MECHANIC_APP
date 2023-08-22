371e8ab73bf33e4e3745444a0004ba5e7dc5e48f
AbdullahM007 <19014156-012@uog.edu.pk>
2023-08-22 07:54:07 +0500

    otp changes

--- src/screens/SmallScreens/OtpScreen.js
+++ src/screens/SmallScreens/OtpScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -6,134 +6,128 @@ import {
   TouchableOpacity,
   StyleSheet,
   Alert,
+  Modal,
 } from 'react-native';
-import {useOtpConfirmationMutation} from '../../ReduxTollKit/Stepney/stepney';
-import {useNavigation} from '@react-navigation/native';
+import { useOtpConfirmationMutation } from '../../ReduxTollKit/Stepney/stepney';
+import { useNavigation } from '@react-navigation/native';
 import OtpInputs from 'react-native-otp-inputs';
-import {useDispatch} from 'react-redux';
-import {storeData} from '../Async/AsyncStorage';
-const OTPScreen = ({route}) => {
+import { useDispatch } from 'react-redux';
+import { storeData } from '../Async/AsyncStorage';
+
+const OTPScreen = ({ route }) => {
   const dispatch = useDispatch();
   const [
     otpConfirmation, // This is the mutation trigger
-    {data, error, isLoading: isUpdating}, // This is the destructured mutation result
+    { data, error, isLoading: isUpdating }, // This is the destructured mutation result
   ] = useOtpConfirmationMutation();
   const navigation = useNavigation();
   const [otp, setOtp] = useState(0);
   const phoneNumber = route?.params?.phNo;
-  const handleOTPChange = text => {
-    // Remove non-numeric characters from the input
-    const numericOTP = text.replace(/[^0-9]/g, '');
-
-    // Add two spaces after every digit
-    const formattedOTP = numericOTP.replace(/(\d)(?=\d)/g, '$1  ');
-
-    // Set the updated OTP value
-    setOtp(formattedOTP);
-  };
-
-  const handleBackspace = () => {
-    // Remove the last digit and spaces (if any) when the backspace is pressed
-    setOtp(prevOtp => prevOtp.replace(/\d?\s?$/, ''));
-  };
 
   const handleVerifyOTP = () => {
     if (otp) {
-      otpConfirmation({contact: phoneNumber, otp: parseInt(otp)});
+      otpConfirmation({ contact: phoneNumber, otp: parseInt(otp) });
     }
     // Implement the logic for verifying the OTP here
     console.log('Verifying OTP:', otp);
     // Add your logic to validate the OTP and navigate to the next screen if it's correct
   };
+
+  // Define the success modal state and visibility
+  const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
+
   React.useEffect(() => {
     if (data) {
-      // storeData('userToken', JSON.stringify(data?.token?.access_token));
-      // dispatch(setToken());
-      // navigation.navigate('RootNavigator');
-    } else {
-      Alert.alert('', 'Invalid otp');
+      // Show the success modal when OTP is successfully verified
+      setIsSuccessModalVisible(true);
+    } else if (error) {
+      Alert.alert('', 'Invalid OTP');
     }
   }, [data, error]);
-  console.log('DFASDFASD', data, error, otp);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Enter OTP</Text>
-      <View style={{height: 100}}>
+      <View style={{ height: 100 }}>
         <OtpInputs
-          handleChange={code => setOtp(code)}
+          handleChange={(code) => setOtp(code)}
           numberOfInputs={6}
-          inputStyles={{
-            height: 60,
-            width: 50,
-            borderWidth: 2,
-            color: '#0A0A0A',
-            marginEnd: 10,
-            borderRadius: 10,
-            fontSize: 26,
-            borderColor: 'rgba(10, 10, 10, 0.2)',
-          }}
-          // style={{backgroundColor: 'green'}}
+          inputStyles={styles.otpInputStyles}
         />
       </View>
-      {/* <View style={styles.otpContainer}>
-        <TextInput
-          style={styles.otpInput}
-          keyboardType="numeric"
-          maxLength={10} // Considering there will be 6 digits and 7 spaces (e.g., "1  2  3  4  5  6")
-          value={otp}
-          onChangeText={handleOTPChange}
-          onKeyPress={({nativeEvent}) => {
-            if (nativeEvent.key === 'Backspace') {
-              handleBackspace();
-            }
-          }}
-        />
-      </View> */}
       <TouchableOpacity style={styles.verifyButton} onPress={handleVerifyOTP}>
         <Text style={styles.verifyButtonText}>Verify OTP</Text>
       </TouchableOpacity>
+
+      {/* Success Modal */}
+      <Modal
+        visible={isSuccessModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setIsSuccessModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>
+              Your Information is submitted and will soon be verified by our Team support.
+            </Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => {
+                setIsSuccessModalVisible(false);
+                navigation.navigate('NextScreen'); // Navigate to the next screen after verification
+              }}
+            >
+              <Text style={styles.modalButtonText}>OK</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  // Your existing styles ...
+
+  otpInputStyles: {
+    height: 60,
+    width: 50,
+    borderWidth: 2,
+    color: '#0A0A0A',
+    marginEnd: 10,
+    borderRadius: 10,
+    fontSize: 26,
+    borderColor: 'rgba(10, 10, 10, 0.2)',
+  },
+
+  // Modal styles
+  modalContainer: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
     backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  modalText: {
+    fontSize: 16,
     marginBottom: 20,
-    color: 'black',
-  },
-  otpContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
-  otpInput: {
-    borderWidth: 1,
-    color: 'black',
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 15,
-    fontSize: 18,
-    width: 150, // Adjust the width as needed
     textAlign: 'center',
   },
-  verifyButton: {
+  modalButton: {
     backgroundColor: '#007bff',
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 5,
+    alignItems: 'center',
   },
-  verifyButtonText: {
-    color: '#fff',
+  modalButtonText: {
+    color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
   },
